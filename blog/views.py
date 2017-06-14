8413818a92de15c0eeb8a19c7d1cc7904badbaa6
Michael Baaske <mikewithak@gmail.com>
2017-06-14 13:35:44 -0700

    add tagging functionality to blog posts

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 
-def post_list(request, category=None):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -19,7 +26,8 @@ def post_list(request, category=None):
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+                                                   'posts': posts,
+                                                   'tag':tag})
 
 
 class PostListView(ListView):
